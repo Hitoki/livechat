108b0577c6cf23c1d37d9964f31e1c2b6b6b6ac7
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-03-22 16:23:50 +0200

    Add test endpoint

--- livechat.py
+++ livechat.py
@@ -16,7 +16,7 @@ def base():
 @app.route('/livechat/ticket/', methods=['POST'])
 def livechat_ticket():
     """ Send new track to Google analytic from LiveChatInc webhooks.
-    (If chat in request date and #Sell in request message
+    (If "chat" in request date and "#Sell" in request message
      with user_type == agent )
 
     :return:
@@ -39,6 +39,25 @@ def livechat_ticket():
             connection.request('POST', '/collect', params)
     return ""
 
+
+@app.route('/livechat/test/', methods=['POST'])
+def test_url():
+    """ Test LiveChat and Google analytic endpoint
+    :return:
+    """
+    params = urllib.parse.urlencode({
+        'v': 1,
+        'tid': 'UA-75377135-1',
+        'cid': request.cookies.get('_GA'),
+        't': 'event',
+        'ec': 'LiveChat Category',
+        'ea': 'Test',
+        'el': 'Test'
+    })
+    connection = http.client.HTTPConnection(
+        'www.google-analytics.com')
+    connection.request('POST', '/collect', params)
+    return ""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
