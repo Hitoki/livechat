bb0b51f079a5eb68d98bff1a1ca1d006366e85ac
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-03-23 18:54:09 +0200

    Update docerfile back to redis

--- livechat.py
+++ livechat.py
@@ -10,8 +10,8 @@ __all__ = ['app', 'base', 'livechat_ticket']
 app = Flask(__name__)
 
 app.config.update(
-    CELERY_BROKER_URL='amqp://guest@localhost//',
-    CELERY_RESULT_BACKEND='amqp://guest@localhost//',
+    CELERY_BROKER_URL='redis://localhost:6379/0',
+    CELERY_RESULT_BACKEND='redis://localhost:6379/0',
     CELERY_ACCEPT_CONTENT=['json'],
     CELERY_TASK_SERIALIZER='json',
     CELERY_RESULT_SERIALIZER='json'
