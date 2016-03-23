2a38def1651e70ac4e76feeb757245fcd893e726
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-03-23 16:42:32 +0200

    Update celery json content

--- livechat.py
+++ livechat.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 
 app.config.update(
     CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
+    CELERY_RESULT_BACKEND='redis://localhost:6379',
+    CELERY_ACCEPT_CONTENT=['json'],
+    CELERY_TASK_SERIALIZER='json',
+    CELERY_RESULT_SERIALIZER='json'
 )
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
