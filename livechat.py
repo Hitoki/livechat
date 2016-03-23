ed212c0f63c61e979693be52c17f111e22624fdb
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-03-23 19:10:32 +0200

    Add celenium

--- livechat.py
+++ livechat.py
@@ -1,8 +1,14 @@
+import logging
 import requests
 import urllib.parse
+import http.client
 
 from flask import Flask, render_template, request
-import http.client
+
+from raven.contrib.flask import Sentry
+from raven import Client
+from raven.contrib.celery import register_signal, register_logger_signal
+
 from celery import Celery
 
 __all__ = ['app', 'base', 'livechat_ticket']
@@ -18,6 +24,14 @@ app.config.update(
 )
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
+
+
+sentry = Sentry(app, dsn='https://368294fb0e6e4739861c08a2bc277212:b25295a8dcb74cdf98ed95dea61ef2e0@app.getsentry.com/71566')
+client = Client('https://368294fb0e6e4739861c08a2bc277212:b25295a8dcb74cdf98ed95dea61ef2e0@app.getsentry.com/71566')
+
+register_logger_signal(client)
+register_signal(client)
+register_logger_signal(client, loglevel=logging.INFO)
 
 
 @celery.task()
@@ -56,6 +70,15 @@ def livechat_ticket():
     google_analytics_task.apply_async(
         args=(request.json, request.cookies.get('_GA')), countdown=3)
     return ""
+
+# @app.route('/livechat/ticket/', methods=['GET'])
+# def livechat_ticket():
+#     """ Send new track to Google analytic from LiveChatInc webhooks.
+#     (If "sales" is in chat tags)
+#     :return: ""
+#     """
+#     google_analytics_task.apply_async(args=({"chat": {"id": "O4RIX0OXRY", "tags": ["test1", "test2"]}}, request.cookies.get('_GA')), countdown=3)
+#     return ""
 
 
 if __name__ == '__main__':
