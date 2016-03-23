6fbea71090327df6f0c11b7f34556390ffdbcf62
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-03-23 19:47:32 +0200

    Update code for test

--- livechat.py
+++ livechat.py
@@ -36,11 +36,10 @@ register_logger_signal(client, loglevel=logging.INFO)
 
 @celery.task()
 def google_analytics_task(data, ga):
-    pass
-    # auth = ('kidomakai@gmail.com', 'd68ed9aac8511fedb315199228bfb03c')
-    # url = 'https://api.livechatinc.com/chats/'+data['chat']['id']+'/'
-    # headers = {"X-API-Version": "2"}
-    # request_data = requests.get(url, headers=headers, auth=auth)
+    auth = ('kidomakai@gmail.com', 'd68ed9aac8511fedb315199228bfb03c')
+    url = 'https://api.livechatinc.com/chats/'+data['chat']['id']+'/'
+    headers = {"X-API-Version": "2"}
+    request_data = requests.get(url, headers=headers, auth=auth)
     #
     # for tag in request_data.json()['tags']:
     #     params = urllib.parse.urlencode({
