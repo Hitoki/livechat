30a3cf15adef3e8ae5ffc659ebf7cfd0640f39b7
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-03-23 19:41:24 +0200

    Update code for test

--- livechat.py
+++ livechat.py
@@ -36,24 +36,25 @@ register_logger_signal(client, loglevel=logging.INFO)
 
 @celery.task()
 def google_analytics_task(data, ga):
-    auth = ('kidomakai@gmail.com', 'd68ed9aac8511fedb315199228bfb03c')
-    url = 'https://api.livechatinc.com/chats/'+data['chat']['id']+'/'
-    headers = {"X-API-Version": "2"}
-    request_data = requests.get(url, headers=headers, auth=auth)
-
-    for tag in request_data.json()['tags']:
-        params = urllib.parse.urlencode({
-            'v': 1,
-            'tid': 'UA-75377135-1',
-            'cid': ga,
-            't': 'event',
-            'ec': 'LiveChat',
-            'ea': tag,
-            'el': data['chat']['id']
-        })
-        connection = http.client.HTTPConnection(
-            'www.google-analytics.com')
-        connection.request('POST', '/collect', params)
+    pass
+    # auth = ('kidomakai@gmail.com', 'd68ed9aac8511fedb315199228bfb03c')
+    # url = 'https://api.livechatinc.com/chats/'+data['chat']['id']+'/'
+    # headers = {"X-API-Version": "2"}
+    # request_data = requests.get(url, headers=headers, auth=auth)
+    #
+    # for tag in request_data.json()['tags']:
+    #     params = urllib.parse.urlencode({
+    #         'v': 1,
+    #         'tid': 'UA-75377135-1',
+    #         'cid': ga,
+    #         't': 'event',
+    #         'ec': 'LiveChat',
+    #         'ea': tag,
+    #         'el': data['chat']['id']
+    #     })
+    #     connection = http.client.HTTPConnection(
+    #         'www.google-analytics.com')
+    #     connection.request('POST', '/collect', params)
 
 
 @app.route('/', methods=['GET', 'POST'])
