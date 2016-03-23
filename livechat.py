532fad77b1a86306e7ab945aa9476689b6c1ae47
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-03-23 19:35:26 +0200

    Update code for test

--- livechat.py
+++ livechat.py
@@ -41,19 +41,19 @@ def google_analytics_task(data, ga):
     headers = {"X-API-Version": "2"}
     request_data = requests.get(url, headers=headers, auth=auth)
 
-    # for tag in request_data.json()['tags']:
-    #     params = urllib.parse.urlencode({
-    #         'v': 1,
-    #         'tid': 'UA-75377135-1',
-    #         'cid': ga,
-    #         't': 'event',
-    #         'ec': 'LiveChat',
-    #         'ea': tag,
-    #         'el': data['chat']['id']
-    #     })
-    #     connection = http.client.HTTPConnection(
-    #         'www.google-analytics.com')
-    #     connection.request('POST', '/collect', params)
+    for tag in request_data.json()['tags']:
+        params = urllib.parse.urlencode({
+            'v': 1,
+            'tid': 'UA-75377135-1',
+            'cid': ga,
+            't': 'event',
+            'ec': 'LiveChat',
+            'ea': tag,
+            'el': data['chat']['id']
+        })
+        connection = http.client.HTTPConnection(
+            'www.google-analytics.com')
+        connection.request('POST', '/collect', params)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -61,24 +61,24 @@ def base():
     return render_template('base.html')
 
 
-@app.route('/livechat/ticket/', methods=['POST'])
-def livechat_ticket():
-    """ Send new track to Google analytic from LiveChatInc webhooks.
-    (If "sales" is in chat tags)
-    :return: ""
-    """
-    google_analytics_task.apply_async(
-        args=(request.get_json(), request.cookies.get('_GA')), countdown=3)
-    return ""
-
-# @app.route('/livechat/ticket/', methods=['GET'])
+# @app.route('/livechat/ticket/', methods=['POST'])
 # def livechat_ticket():
 #     """ Send new track to Google analytic from LiveChatInc webhooks.
 #     (If "sales" is in chat tags)
 #     :return: ""
 #     """
-#     google_analytics_task.apply_async(args=({"chat": {"id": "O4RIX0OXRY", "tags": ["test1", "test2"]}}, request.cookies.get('_GA')), countdown=3)
+#     google_analytics_task.apply_async(
+#         args=(request.get_json(), request.cookies.get('_GA')), countdown=3)
 #     return ""
+
+@app.route('/livechat/ticket/', methods=['GET', 'POST'])
+def livechat_ticket():
+    """ Send new track to Google analytic from LiveChatInc webhooks.
+    (If "sales" is in chat tags)
+    :return: ""
+    """
+    google_analytics_task.apply_async(args=({"chat": {"id": "O4RIX0OXRY", "tags": ["test1", "test2"]}}, request.cookies.get('_GA')), countdown=3)
+    return ""
 
 
 if __name__ == '__main__':
