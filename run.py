0bef0e6533066971dc1a54a36bcea232926034b2
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-03-29 17:50:33 +0300

    Add test account

--- run.py
+++ run.py
@@ -9,3 +9,9 @@ if not User.query.filter_by(username='admin').first():
     admin = User(username='admin', password='admin')
     db.session.add(admin)
     db.session.commit()
+
+
+if not User.query.filter_by(username='test').first():
+    test = User(username='test', password='test')
+    db.session.add(test)
+    db.session.commit()
