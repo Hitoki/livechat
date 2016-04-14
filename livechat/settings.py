2157660af0abbe91f7e90cc9ede8f2ce0bb35be4
Vitaliy Romanuik <vitaliy.romanuik@anvil8.com>
2016-04-14 18:48:13 +0300

    Add postgressql

--- livechat/settings.py
+++ livechat/settings.py
@@ -4,18 +4,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
-        basedir, 'livechat.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
+    #     basedir, 'livechat.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'pofk84z0bl9g14k0f'
 
 
 class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "postgresql://livechat"
     DEBUG = False
     TESTING = False
 
 
 class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "postgresql:///livechat"
     DEBUG = True
     TESTING = False
     GOOGLE_TRACK_ID = 'UA-75377135-1'
---
+++ manage.py
@@ -0,0 +1,16 @@
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+from livechat import app, db
+
+
+app.config.from_object('livechat.settings.DevelopmentConfig')
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
